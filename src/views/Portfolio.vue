d5d0752ea527d0c3c4e4d7e99c6ba2ac78e7ea16
Losev Andrey <Losikoban@Gmail.com>
2019-08-20 22:42:22 +0300

    init

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>
+        Портфолио
+    </h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'portfolio'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/_/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar navbar-is-fixed">
+        <ul>
+            <li>
+                <router-link to="/">Главная</router-link>
+            </li>
+            <li>
+                <router-link to="/about">Обо мне</router-link>
+            </li>
+            <li>
+                <router-link to="/portfolio">Портфолио</router-link>
+            </li>
+            <li>
+                <router-link to="/reviews">Отзывы</router-link>
+            </li>
+            <li>
+                <router-link to="/contacts">Контакты</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar'
+
+}
+</script>
